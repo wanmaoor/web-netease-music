4e173126ace352e9fb77428e844be3eef87fcb24
wanmao <wequart@gmail.com>
2021-03-16 10:21:52 +0800

    📦 NEW: 完成左右布局

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,11 +1,25 @@
-import Nav from "../../components/Nav/Nav";
-
+import Nav from '../../components/Nav/Nav';
+import SplitPane from 'react-split-pane';
+import './index.scss';
+import LeftPanel from '../LeftPanel';
+import RightPanel from '../RightPanel';
 const Main = () => {
   return (
     <>
       <Nav />
+      <div className="Resizer" style={{ marginTop: 60 }}>
+        <SplitPane
+          split="vertical"
+          minSize={200}
+          maxSize={600}
+          allowResize={true}
+        >
+          <LeftPanel />
+          <RightPanel />
+        </SplitPane>
+      </div>
     </>
   );
-}
+};
 
-export default Main
+export default Main;
