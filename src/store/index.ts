7880b3193e5c5e5fed52ad80e76dab6ee7de7959
wanmao <wequart@gmail.com>
2021-03-16 11:28:22 +0800

    📦 NEW: 配置saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,11 @@
-import {createStore} from "redux";
+import {createStore, compose, applyMiddleware} from "redux";
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './saga'
 import reducer from "./reducers";
-const store = createStore(reducer)
+
+const composeEnhancers = compose
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reducer, composeEnhancers(applyMiddleware(sagaMiddleware)))
+sagaMiddleware.run(rootSaga)
 
 export default  store
